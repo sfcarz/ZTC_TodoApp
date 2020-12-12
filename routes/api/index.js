01b6e8acb445a4bdd02ea72298478bb1e3fac083
sfcarz <chrisarojasz2003@gmail.com>
2020-12-12 13:47:09 -0800

    connected, routes, controller. Postman and mongodb connected

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
-const toDoController = require('../../controller/toDoController');
+const addTodo = require('../../controller/toDoController');
+const User = require('../../controller/userController');
 
-router.post('/addTodo', toDoController.addTodo)
+router.post('/addTodo', addTodo);
+
+router.route('/').post(User);
+router.route('/').get(User);
+router.route('/:Id').get(User);
+router.route('/:Id').patch(User);
+router.route('/:Id').delete(User);
+// router.get('/user', User);
+
+// router.get('/user', User)
+// router.get('/user/:id', User)
+// router.post('/user', User)
+// router.delete('/user', User)
+// router.patch('/user', User)
 
 module.exports = router 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();	
-const apiRoutes = require('./api');	
+const apiRoutes = require('./api/index');	
 const authRoutes = require('./authRoutes');	
 // /api/users	
-router.use('/api', apiRoutes);	
-router.use('/auth', authRoutes);	
+router.use('/user', apiRoutes);	
+router.use('/auth', authRoutes);
+
+router.get('/', (req, res) => {
+  res.send('This is from Routes index.js')
+});
 
 
 module.exports = router;
