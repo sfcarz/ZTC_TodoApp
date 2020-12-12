d12190dcefc5a31b7f09f6346d1b1beb4448f1aa
Tara Dehdari <tara.dehdari@gmail.com>
2020-12-11 19:30:09 -0800

    the start of the todo style page, made background boxes for the weather and checklist portion

--- client/src/components/weather.js
+++ client/src/components/weather.js
@@ -1,13 +1,20 @@
 import { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Weather extends Component{
     render() {
         return(
-            <div>
-                <h1>Weather portion</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <div className="banner-text">
+                            <h1>Hi, Welcome to you ToDo App (USERNAME)</h1>
+                            <h3>What would you like do today?</h3>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
-
     }
 }
 
