5831a33ea1b7069c800be1b4013074cdc58d7933
Tara Dehdari <tara.dehdari@gmail.com>
2020-12-16 11:14:49 -0800

    made the temp condiition into actual numbers not strings

--- client/src/components/weather.js
+++ client/src/components/weather.js
@@ -11,7 +11,7 @@ class Weather extends Component {
             <div className="banner-text-weather">
               <h1>Hi, Welcome to you ToDo App (USERNAME)</h1>
               <div>
-                <WeatherCard temp="5" />
+                <WeatherCard temp={-20} />
               </div>
             </div>
           </Cell>
